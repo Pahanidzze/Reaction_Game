96ece774ca10cc9364caefd01587119463c08445
Pavel Telemdzhiev <0pahan001@gmail.com>
2023-07-23 19:56:48 +0500

    Cyrcle color

--- Program.cs
+++ Program.cs
@@ -11,27 +11,27 @@ namespace Reaction_Game
 {
     class Program : Game
     {
-        static uint winH = 600;
-        static uint winW = 800;
-
-        static string BGM = LoadMusic("bg_music.wav");
+        static string backGroundMusic = LoadMusic("bg_music.wav");
 
         static void Main()
         {
-            InitWindow(winW, winH, "Window");
+            uint winWeight = 800;
+            uint winHeihgt = 600;
+            InitWindow(winWeight, winHeihgt, "Window");
             SetFont("comic.ttf");
-            PlayMusic(BGM, 20);
+            PlayMusic(backGroundMusic, 20);
             Game();
         }
 
         static unsafe void Game()
         {
             bool active;
-            int innerSize;
-            int outerSize;
+            int innerCyrcleSize;
+            int outerCyrcleSize;
             int score = 0;
             int highScore = 0;
-            ResetParameters(&active, &innerSize, &outerSize, &score, &highScore);
+            Color innerCyrcleColor = ColorSwitch(Color.Black);
+            ResetParameters(&active, &innerCyrcleSize, &outerCyrcleSize, &score, &highScore);
             bool keyPressed = false;
             while(true)
             {
@@ -42,26 +42,30 @@ namespace Reaction_Game
                     {
                         active = true;
                         keyPressed = true;
-                    } else if (Keyboard.IsKeyPressed(Keyboard.Key.Space))
-                    {
-                        outerSize = innerSize;
-                        innerSize = 0;
-                        keyPressed = true;
                     }
-                } else if (!Keyboard.IsKeyPressed(Keyboard.Key.Space))
+                    else if (Keyboard.IsKeyPressed(Keyboard.Key.Space))
+                    {
+                        outerCyrcleSize = innerCyrcleSize;
+                        innerCyrcleSize = 0;
+                        keyPressed = true;
+                        innerCyrcleColor = ColorSwitch(innerCyrcleColor);
+                    }
+                }
+                else if (!Keyboard.IsKeyPressed(Keyboard.Key.Space))
                 {
                     keyPressed = false;
                 }
-                if (innerSize >= outerSize)
+                if (innerCyrcleSize >= outerCyrcleSize)
                 {
-                    ResetParameters(&active, &innerSize, &outerSize, &score, &highScore);
-                } else if (active)
+                    ResetParameters(&active, &innerCyrcleSize, &outerCyrcleSize, &score, &highScore);
+                }
+                else if (active)
                 {
                     score++;
-                    innerSize++;
+                    innerCyrcleSize++;
                 }
                 ClearWindow();
-                FillGame(active, Color.Red, score, highScore, innerSize, outerSize);
+                FillGame(active, innerCyrcleColor, score, highScore, innerCyrcleSize, outerCyrcleSize);
                 DisplayWindow();
                 Delay(1);
             }
@@ -85,7 +89,8 @@ namespace Reaction_Game
             if (!active)
             {
                 DrawText(20, 120, "Начать игру: ПРОБЕЛ", 21);
-            } else
+            }
+            else
             {
                 DrawText(20, 120, "Остановить круг: ПРОБЕЛ", 21);
             }
@@ -94,6 +99,24 @@ namespace Reaction_Game
             FillCircle(500, 330, outerSize);
             SetFillColor(innerColor);
             FillCircle(500, 330, innerSize);
+        }
+
+        static Color ColorSwitch(Color color)
+        {
+            var Rand = new Random();
+            Color newColor = color;
+            int newColorIndex;
+            while(newColor == color)
+            {
+                newColorIndex = Rand.Next(6);
+                if (newColorIndex == 0) newColor = Color.Blue;
+                else if (newColorIndex == 1) newColor = Color.Green;
+                else if (newColorIndex == 2) newColor = Color.Red;
+                else if (newColorIndex == 3) newColor = Color.Cyan;
+                else if (newColorIndex == 4) newColor = Color.Magenta;
+                else newColor = Color.Yellow;
+            }
+            return newColor;
         }
     }
 }
